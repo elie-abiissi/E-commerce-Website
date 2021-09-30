5685da257808f0b64781b431e8438a09f79b9340
elie-abiissi <84697459+elie-abiissi@users.noreply.github.com>
2021-09-30 13:34:15 +0300

    Add files via upload

---
+++ products/addorder.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+    if(isset($_GET['prod']) && isset($_SESSION['id']) && isset($_SESSION['cart']) &&             isset($_GET['quantity'])){
+        if(!empty($_GET['prod']) && !empty($_SESSION['id']) && !empty($_SESSION['cart']) && 
+        !empty(isset($_GET['quantity']))){
+            $client =  $_SESSION['id'];
+            $cart = $_SESSION['cart'];
+            $product = $_GET['prod'];
+            $quantity = $_GET['quantity'];
+
+            $link = mysqli_connect ('localhost', 'root', '','web3_project')
+        or die(mysqli_connect_errno()." : ".mysqli_connect_error ( ));
+$query = "INSERT INTO orders (idproduct, quantity, idcart) VALUES ($product, $quantity, $cart)";
+$result = mysqli_query($link,$query);
+
+$query = "UPDATE products SET stock = stock - $quantity WHERE idproduct = $product";
+$result = mysqli_query($link,$query);
+        }
+    }
+?>
